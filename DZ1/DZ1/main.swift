eb09458a565f35d90011cce5791b043148eb4920
Yury Shashkin <yshashkin@yandex.ru>
2018-09-16 13:25:13 +0300

    first commit

---
+++ DZ1/DZ1/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  DZ1
+//
+//  Created by Юрий Шашкин on 16.09.2018.
+//  Copyright © 2018 Юрий Шашкин. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
